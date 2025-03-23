2704f9c3be158328cfb0e9cb093d938255bacb42
Ilya-ve <bt2303@tec.ukr.education>
2025-03-24 01:40:46 +0200

    .

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 import 'bootstrap';
 
 document.addEventListener("DOMContentLoaded", function () {
-  const currentUrl = window.location.pathname.replace('/', '');
+  const currentUrl = window.location.pathname.split('/')[1];
   document.querySelectorAll('header .nav-link').forEach(function (link) {
     if (currentUrl.startsWith(link.getAttribute('href'))) {
       link.classList.add("active");
