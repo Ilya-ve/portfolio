8dc484162a9684f2412d0ef34317d066d7da3b07
Dima <admin@admins-MacBook-Pro.local>
2025-03-23 23:39:58 +0200

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+import { isWebp } from './modules'
+
+window['FLS'] = true
+
+/* Проверка поддержки webp, добавление класса webp или no-webp для HTML
+! (i) необходимо для корректного отображения webp из css 
+*/
+isWebp()
+
