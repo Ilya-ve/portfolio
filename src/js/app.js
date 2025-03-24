fbede6359e3167c97354e68a4068fdec04fbcc9a
Ilya-ve <bt2303@tec.ukr.education>
2025-03-24 11:28:02 +0200

    .

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,20 @@
 import 'bootstrap';
 
 document.addEventListener("DOMContentLoaded", function () {
+  headerLinks();
+});
+
+function headerLinks() {
   const currentUrl = window.location.pathname.split('/')[1];
+
+  if (!currentUrl) {
+    document.querySelector('header .nav-link').classList.add("active");
+    return;
+  }
+
   document.querySelectorAll('header .nav-link').forEach(function (link) {
     if (currentUrl.startsWith(link.getAttribute('href'))) {
       link.classList.add("active");
     }
   });
-});
-
+}
